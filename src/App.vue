cf75efe37ed2e3bc317e75e9bbddb4e53bb5ef59
Davy Jones <53236741+davyjonesdesign@users.noreply.github.com>
2023-05-25 17:21:25 -0500

    Setup Basic Blog App
    structure & styling

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,121 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <Logo></Logo>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
+<script>
+import Logo from "@/components/Logo.vue";
 
+export default {
+  components: {
+    Logo,
+  },
+}
+</script>
 <style lang="scss">
+@import "@/styles/main.scss";
+html {
+  background: $off;
+  margin: 0;
+}
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $text;
+  letter-spacing: 0;
+  font-family: "Raleway", sans-serif;
+  max-width: 100vw;
+  margin: 0 auto;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+p,
+table,
+ul,
+ol,
+li {
+  font-family: "Lato", sans-serif;
+  color: $text;
 }
+h1 {
+  font-weight: 900;
+  font-size: 4rem;
+  margin-bottom: 20px;
+  // letter-spacing: 2px;
+  line-height: 3.5rem;
+}
+h2 {
+  font-weight: 500;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  letter-spacing: 10px;
+}
+h3 {
+  font-size: 1.35rem;
+  line-height: 1.75rem;
+  font-weight: 600;
+  margin-top: 40px;
+}
+h4 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  line-height: 1.5;
+  letter-spacing: 3px;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+}
+h5 {
+  font-size: 1.1rem;
+  font-weight: 400;
+  margin-bottom: 10px;
+}
+h6 {
+  font-size: 0.75rem;
+  font-weight: 500;
+  letter-spacing: 4px;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+}
+p {
+  margin: 0 0 25px;
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: $text;
+  max-width: 600px;
+}
+a {
+  border: none;
+  font-weight: 600;
+  letter-spacing: 0.25px;
+  font-family: "Lato", sans-serif;
+  text-decoration: none;
+  color: $heavy-op;
+}
+input,
+label,
+textarea {
+  font-family: "Lato", sans-serif;
+}
+ul,
+ol {
+  padding: 5px 0 10px 20px;
+  font-weight: 400;
+  line-height: 1.25rem;
+}
+li {
+  padding-bottom: 15px;
+}
+.page {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  margin-left: 60px;
+}
+@import "@/styles/media.scss";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,34 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "../views/Home.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+Vue.use(Router);
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+import BlogEntries from "../statics/data/blogs.json";
+
+const blogRoutes = Object.keys(BlogEntries).map(section => {
+  const children = BlogEntries[section].map(child => ({
+    path: child.id,
+    name: child.id,
+    component: () => import(`../markdowns/${section}/${child.id}.md`)
+  }));
+  return {
+    path: `/${section}`,
+    name: section,
+    component: () => import("../views/Blog.vue"),
+    children
+  };
 });
 
-export default router;
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    ...blogRoutes
+  ]
+});
